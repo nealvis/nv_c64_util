7f569c9e133bb49c88430f4cbc0958534c801d50
Neal Smith <nealvis@gmail.com>
2021-10-02 17:20:47 -0700

    added consts for processor flag bits

--- nv_c64_util_macs.asm
+++ nv_c64_util_macs.asm
@@ -12,7 +12,7 @@
 // verify that the nv_c64_util_data.asm file is imported some where
 // because the _macs.asm files will depend on this.
 #if !NV_C64_UTIL_DATA
-.error "Error - nv_c64_util_macs.asm: NV_C64_UTIL_DATA not defined.  Import nv_c64_util_data.asm"
+//.error "Error - nv_c64_util_macs.asm: NV_C64_UTIL_DATA not defined.  Import nv_c64_util_data.asm"
 #endif
 
 // the #if above doesn't seem to always work so..
@@ -22,6 +22,7 @@
 // Import all the _macs.asm files here.  They will not generate any
 // code or data just by importing them, but will make the macros
 // available to use
+#import "nv_processor_status_macs.asm"
 #import "nv_branch16_macs.asm"
 #import "nv_branch8_macs.asm"
 #import "nv_color_macs.asm"
---
+++ nv_processor_status_macs.asm
@@ -0,0 +1,30 @@
+//////////////////////////////////////////////////////////////////////////////
+// nv_processor_status_macs.asm
+// Copyright(c) 2021 Neal Smith.
+// License: MIT. See LICENSE file in root directory.
+//////////////////////////////////////////////////////////////////////////////
+// This assembler file contains macros and constants regarding the 
+// processor status register and maybe used throughout the 
+// nv_c64_utils project
+
+#importonce
+
+#if !NV_C64_UTIL_DATA
+//.error "Error - nv_processor_status_macs.asm: NV_C64_UTIL_DATA not defined.  Import nv_c64_util_data.asm"
+#endif
+
+// the #if above doesn't seem to always work so..
+// if data hasn't been imported yet, import it into default location
+#importif !NV_C64_UTIL_DATA "nv_c64_util_default_data.asm"
+
+
+// the 6502 processor status flag bits 
+.const NV_PROCSTAT_NONE = $00
+.const NV_PROCSTAT_CARRY = $01
+.const NV_PROCSTAT_ZERO = $02
+.const NV_PROCSTAT_INTERUPT = $04
+.const NV_PROCSTAT_DECIMAL = $08
+.const NV_PROCSTAT_BREAK = $10
+.const NV_PROCSTAT_UNKNOWN = $20
+.const NV_PROCSTAT_OVERFLOW = $40
+.const NV_PROCSTAT_NEGATIVE = $80
